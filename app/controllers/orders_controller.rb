4a7569c6ba01492022c4210718a7cb8ad353a7dc
Perry Defayette <defayetteperry@gmail.com>
2021-03-03 23:47:41 +0000

    Add order-details info to order page.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,9 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @items = @order.line_items.all
+    puts @items
+    puts @order.email
   end
 
   def create
